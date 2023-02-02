a091cc8a55100eb0d08be771a6edf74e18011eee
FranklinProtazio <franklinprotazio@gmail.com>
2023-02-01 23:01:07 -0300

    Aula 44

--- exercicios/src/fundamentos/Import.java
+++ exercicios/src/fundamentos/Import.java
@@ -1,20 +1,34 @@
 package fundamentos;
 
 import java.util.Date;
+import java.util.Scanner;
 
 public class Import {
 
 	public static void main(String[] args) {
-		java.lang.String b = "Boa tarde!";
-		java.lang.System.out.println(b);
-		
-		String s = "Bom dia!!";
-		System.out.println(s);
-		
-		Date d = new Date();
-		System.out.println(d);	
+//		java.lang.String b = "Boa tarde!";
+//		java.lang.System.out.println(b);
+//		
+//		String s = "Bom dia!!";
+//		System.out.println(s);
+//		
+//		Date d = new Date();
+//		System.out.println(d);	
 		
 		// JButton botao = new JButton();
+		
+		Scanner teclado = new Scanner(System.in);
+		 
+	    System.out.print("Qual a sua idade?");
+	    int idade = teclado.nextInt();
+	//    teclado.nextLine(); //lê o "\\\\n" que o teclado.nextInt() deixa para trás.
+
+	    System.out.print("Qual o seu nome?");
+	    String nome = teclado.nextLine();
+	    System.out.print("Qual o seu sobrenome?");
+	    String sobrenome = teclado.nextLine();
+	 
+	    teclado.close();
 		
 	}
 
